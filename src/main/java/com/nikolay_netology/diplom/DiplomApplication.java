b47f587d9989d2ac355fc7ffebebf6300090d064
nik86olay <nik86olay@gmail.com>
2023-06-21 13:58:57 +0300

    Initial commit

---
+++ src/main/java/com/nikolay_netology/diplom/DiplomApplication.java
@@ -0,0 +1,13 @@
+package com.nikolay_netology.diplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikolay_netology/diplom/DiplomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolay_netology.diplom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
